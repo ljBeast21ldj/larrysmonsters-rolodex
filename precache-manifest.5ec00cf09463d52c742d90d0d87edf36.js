e5afc4524b66c548dfa48dc242e82d29e9ffcd9a
beast-box <larryd.soularian@gmail.com>
2020-09-10 21:20:21 -0400

    Updates

---
+++ precache-manifest.5ec00cf09463d52c742d90d0d87edf36.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2489c053954a3709f2ddbe5ca3aa3c93",
+    "url": "/larrysmonsters-rolodex/index.html"
+  },
+  {
+    "revision": "4ef52bfe62bf72264fcc",
+    "url": "/larrysmonsters-rolodex/static/css/main.3388d3af.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/larrysmonsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/larrysmonsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ef52bfe62bf72264fcc",
+    "url": "/larrysmonsters-rolodex/static/js/main.28de8c6e.chunk.js"
+  },
+  {
+    "revision": "dc90e052709f5d4aa25d",
+    "url": "/larrysmonsters-rolodex/static/js/runtime-main.61ffdbd5.js"
+  }
+]);
